9f694436db57be90366e1953f4fe4b7784ff72a3
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-11 10:06:32 -0300

    feat: BotTab from MenuBar finished

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -2,55 +2,70 @@ import React from 'react';
 
 import Button from '../Button';
 
-import { 
-    Container,
-    TopSide,
-    Logo,
-    MenuButtons,
-    HomeIcon,
-    BellIcon,
-    EmailIcon,
-    FavoriteIcon,
-    ProfileIcon,
+import {
+  Container,
+  TopSide,
+  Logo,
+  MenuButtons,
+  HomeIcon,
+  BellIcon,
+  EmailIcon,
+  FavoriteIcon,
+  ProfileIcon,
+  BotSide,
+  Avatar,
+  ProfileData,
+  ExitIcon,
 } from './styles';
 
 const MenuBar: React.FC = () => {
   return (
-      <Container>
-          <TopSide>
-              <Logo />
+    <Container>
+      <TopSide>
+        <Logo />
 
-              <MenuButtons>
-                  <HomeIcon />
-                  <span>Página inicial</span>
-              </MenuButtons>
+        <MenuButtons>
+          <HomeIcon />
+          <span>Página inicial</span>
+        </MenuButtons>
 
-              <MenuButtons>
-                  <BellIcon />
-                  <span>Notificações</span>
-              </MenuButtons>
+        <MenuButtons>
+          <BellIcon />
+          <span>Notificações</span>
+        </MenuButtons>
 
-              <MenuButtons>
-                  <EmailIcon />
-                  <span>Mensagens</span>
-              </MenuButtons>
+        <MenuButtons>
+          <EmailIcon />
+          <span>Mensagens</span>
+        </MenuButtons>
 
-              <MenuButtons>
-                  <FavoriteIcon />
-                  <span>Favoritados</span>
-              </MenuButtons>
+        <MenuButtons>
+          <FavoriteIcon />
+          <span>Favoritados</span>
+        </MenuButtons>
 
-              <MenuButtons className='active'>
-                  <ProfileIcon />
-                  <span>Perfil</span>
-              </MenuButtons>
+        <MenuButtons className="active">
+          <ProfileIcon />
+          <span>Perfil</span>
+        </MenuButtons>
 
-              <Button>
-                  <span>Tweetar</span>
-              </Button>
-          </TopSide>
-      </Container>
+        <Button>
+          <span>Tweetar</span>
+        </Button>
+      </TopSide>
+
+      <BotSide>
+        <Avatar />
+
+        <ProfileData>
+          <strong>Matheus Santos</strong>
+          <span>@mathpsantos</span>
+        </ProfileData>
+
+        <ExitIcon />
+      </BotSide>
+    </Container>
   );
-}
+};
 
 export default MenuBar;
