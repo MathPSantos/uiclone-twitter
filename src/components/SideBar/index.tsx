e13bbf6b00a7a2da5f815a2a49877723633a83f9
Matheus Santos <matheus.psantos2016@gmail.com>
2020-07-11 10:40:49 -0300

    feat: FollowSugestion component added to SideBar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import { 
-    Container,
-    SearchWrapper,
-    SearchInput,
-    SearchIcon,
-    Body, 
+import List from '../List';
+import FollowSugestion from '../FollowSugestion';
+
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
 } from './styles';
 
 const SideBar: React.FC = () => {
@@ -17,7 +20,58 @@ const SideBar: React.FC = () => {
       </SearchWrapper>
 
       <Body>
-        <p>{'lorem ipsum dolor sit amet.'.repeat(90)}</p>
+        <List 
+          title="Talvez você curta" 
+          elements={[
+            <FollowSugestion 
+              name="Deni Pires"
+              nickname="@deni_pires"
+            />,
+            <FollowSugestion 
+              name="Alberto Santos"
+              nickname="@alberto_santos"
+            />,
+            <FollowSugestion 
+              name="Simone Silva"
+              nickname="@sinsilva"
+            />,
+            <FollowSugestion 
+              name="Marcos Roberto"
+              nickname="@marquinhosdenete"
+            />,
+            <FollowSugestion 
+              name="Monique Pires"
+              nickname="@mohpires"
+            />,
+            <FollowSugestion 
+              name="Vitor Souza"
+              nickname="@vitor2008"
+            />,
+            <FollowSugestion 
+              name="Giovanna Avi"
+              nickname="@gihgihavi"
+            />,
+            <FollowSugestion 
+              name="Josias Calebe"
+              nickname="@jcalebe"
+            />,
+            <FollowSugestion 
+              name="Helena Strada"
+              nickname="@hroad"
+            />,
+          ]} 
+        />
+
+        <List
+          title="Talvez você curta"
+          elements={[
+            <h1>Test</h1>,
+            <h1>Test</h1>,
+            <h1>Test</h1>,
+            <h1>Test</h1>,
+            <h1>Test</h1>,
+          ]}
+        />
       </Body>
     </Container>
   );
